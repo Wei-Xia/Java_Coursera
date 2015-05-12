0f62eba5a48ac68a513972b3beb547d7f12d2edf
Wei-Xia <xw900812@gmail.com>
2015-05-12 14:45:13 -0400

    \t in Java

---
+++ Table.java
@@ -0,0 +1,11 @@
+
+public class Table {
+	
+	public static void main(String[] args) {
+
+		System.out.println("123\t456");
+		System.out.println("12\t456");
+		System.out.println("1\t456");
+			
+	}
+}
