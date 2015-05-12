66f1b3541ea8447366a3000e27f59087577a2196
Wei-Xia <xw900812@gmail.com>
2015-05-11 22:32:54 -0400

    Print_Out

---
+++ Read.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Read {
+
+	public static void main(String[] args) {
+		int bill = 100;
+
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println(in.nextLine()); //read next line and print out the whole sentence
+
+		System.out.println(in.next());  //read next line and print out first word
+		
+
+	}
+}
