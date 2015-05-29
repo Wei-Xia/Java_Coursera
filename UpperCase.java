9f0b63a2b4422414e6b413aa4c496ac7d06e4bb5
Wei-Xia <xw900812@gmail.com>
2015-05-28 21:13:32 -0400

    UpperCase

---
+++ UpperCase.java
@@ -0,0 +1,20 @@
+//change first input word into Upper case
+
+import java.util.*;
+
+public class Three {
+
+  public static void main(String[] args){
+    Scanner in = new Scanner (System.in);
+    String s = in.nextLine();
+    System.out.println(s);  //output the input word
+
+    char ch  = s.charAt(0);
+    if (Character.isLowerCase(ch))
+    {
+        ch = Character.toUpperCase(ch);
+    }
+    System.out.println(ch);
+
+  }
+}
