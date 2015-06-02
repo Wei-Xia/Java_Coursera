b72c21c8fe0a009d1907177b1f99fbfbbfd1d7dc
Wei-Xia <xw900812@gmail.com>
2015-06-01 23:00:30 -0400

    UpperCase
    UpperCase all the input words

---
+++ UpperCase_Method.java
@@ -0,0 +1,16 @@
+//UpperCase all the input words
+
+public class UpperCase{
+
+public static void UpperCase(String s){	
+	System.out.println(s);
+	System.out.println(s.toUpperCase());
+}
+
+public static void main(String[] args) {
+	String s= "hello";
+	UpperCase(s);
+}
+
+}
+
